38f0da9a74dca9e8419f044326c09766daa50413
Soup Cat <soupcat.py@gmail.com>
2025-07-01 19:54:18 -0600

    01 Jul 19:53
    started the very beginning of JS (it's hard)

---
+++ script.js
@@ -0,0 +1,13 @@
+// damn JS might be hard to learn
+// anyway
+
+// i heard this was good to avoid having bad practise
+// it raises errors when i try to use an undeclared variable
+"use strict"
+
+// define entries
+const lat1_box = document.getElementById('lat1');
+const long1_box = document.getElementById('long1')
+const lat2_box = document.getElementById('lat2')
+const long2_box = document.getElementById('long2')
+const dist_box = document.getElementById('dist')
